8306b3efdc0bb63fc141282fbcf0f41c1d375dc2
Josh Franey <josh.franey@outlook.com>
2017-08-24 21:01:02 -0400

    Completed Week3 work, and updated Week-3.md.

---
+++ Lunch/Controllers/PersonController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Lunch.Models;
+
+namespace Lunch.Controllers
+{
+    public class PersonController : Controller
+    {
+        // GET: Person
+        public ActionResult Index()
+        {
+            var personList = new PersonListViewModel
+            {
+                People = new List<PersonViewModel>
+                {
+                    new PersonViewModel { PersonId = 1, LastName = "Gibbons", FirstName = "Peter" },
+                    new PersonViewModel { PersonId = 2, LastName = "Bolton", FirstName = "Michael" },
+                    new PersonViewModel { PersonId = 3, LastName = "Nagheenanajar", FirstName = "Samir"},
+                    new PersonViewModel { PersonId = 4, LastName = "Smykowski", FirstName = "Tom" },
+                    new PersonViewModel { PersonId = 5, LastName = "Waddams", FirstName = "Milton" },
+                    new PersonViewModel { PersonId = 6, LastName = "Lumbergh", FirstName = "Bill" }
+                }
+            };
+
+            personList.TotalPeople = personList.People.Count;
+
+            return View(personList);
+        }
+    }
+}
---
+++ Lunch/Models/PersonListViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Lunch.Models
+{
+    public class PersonListViewModel
+    {
+        public List<PersonViewModel> People { get; set; }
+        public int TotalPeople { get; set; }
+    }
+}

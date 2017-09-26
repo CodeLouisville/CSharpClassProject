e5c19d70c4b19c6b7a2cde23291575b09757db1d
Josh Franey <josh.franey@outlook.com>
2017-09-25 23:45:15 -0400

    Followed instructinos (so far) from Week-4.md in Week4 branch

---
+++ Lunch/Models/Person.cs
@@ -0,0 +1,9 @@
+﻿namespace Lunch.Models
+{
+    public class Person
+    {
+        public int PersonId { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+    }
+}
--- Lunch/Models/PersonViewModel.cs
+++ Lunch/Models/PersonViewModel.cs
@@ -1,10 +1,18 @@
-﻿namespace Lunch.Models
+﻿using System.ComponentModel;
+
+namespace Lunch.Models
 {
     public class PersonViewModel
     {
         public int? PersonId { get; set; }
+
+        [DisplayName("Last Name")]
         public string LastName { get; set; }
+
+        [DisplayName("First Name")]
         public string FirstName { get; set; }
+
+        [DisplayName("Name")]
         public string FullName => FirstName + " " + LastName;
     }
 }
